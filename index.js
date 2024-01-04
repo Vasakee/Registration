431fcc419cd37000b6d08bf694eb77ce94ff9ec1
BASIL AST <basilast@BASILs-MacBook-Pro.local>
2024-01-05 00:26:20 +0100

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 const app = express()
 dotenv.config()
-mongoose.connect(process.env.MONGO_URI,)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log(`connected to mongoDB`)
     }).catch((error) => {
@@ -37,3 +37,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(PORT, console.log(`server is running on Port ${PORT}`))
+
